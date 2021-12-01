1c027fd8644ac63c38a4b20ba17d35f531403baf
mawsyh <moh.mir36@gmail.com>
2021-12-01 13:31:51 +0330

    Q1-2 done

---
+++ Q1-2/main.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const port = 4000;
+const testJsonFile = {
+  name: "Mina",
+  familyName: "Alipoor",
+  age: "52",
+  job: "Nurse",
+};
+
+const httpServer = http.createServer((req, res) => {
+  if (req.url === "/api/json") {
+    res.write(JSON.stringify(testJsonFile));
+    res.end();
+  }
+});
+
+httpServer.listen(port);
+console.log("Listening on port " + port);
