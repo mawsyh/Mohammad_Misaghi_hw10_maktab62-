67e446428ab5ca4ce07bd71181de1419051f948c
mawsyh <moh.mir36@gmail.com>
2021-12-01 19:18:35 +0330

    Q1-5 done

---
+++ Q1-5/main.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const http = require("http");
+const port = 4000;
+let htmlPage;
+try {
+  htmlPage = fs.readFileSync("./index.html", "utf-8");
+} catch (e) {
+  console.log(e);
+}
+
+const httpServer = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write(htmlPage);
+    res.end();
+  }
+});
+
+httpServer.listen(port);
+console.log("Listening on port " + port);
